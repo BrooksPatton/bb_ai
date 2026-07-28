13ad69f69ce76d2d2aa25fdca33cf40c8559da8a
Brooks Patton <brooks@brookzerker.com>
2026-07-28 15:32:19 -0700

    anathema hello world up and running

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 - [ ] Simple chat
 - [ ] LinkedIn account supporter
+- [ ] Programming error lookup
 
 ## Skills / Abilities
 
 - [ ] /loop, for routinely running a task over and over again over a period of time
+
---
+++ tui/src/main.rs
@@ -0,0 +1,23 @@
+// src/main.rs
+use anathema::prelude::{Backend, Document, TuiBackend};
+use anathema::runtime::Runtime;
+
+fn main() {
+    let doc = Document::new("@index");
+
+    let mut backend = TuiBackend::builder()
+        .enable_alt_screen()
+        .enable_raw_mode()
+        .hide_cursor()
+        .finish()
+        .unwrap();
+    backend.finalize();
+
+    let mut builder = Runtime::builder(doc, &backend);
+    builder
+        .default::<()>("index", "templates/index.aml")
+        .unwrap();
+    builder
+        .finish(&mut backend, |runtime, backend| runtime.run(backend))
+        .unwrap();
+}
